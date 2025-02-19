1e4a76fe2cb93128eaf678411d315fd5c9324218
ben_gorman <bengorman1994@gmail.com>
2025-02-20 06:49:11 +1100

    feat: server error handling

--- src/server.js
+++ src/server.js
@@ -9,6 +9,25 @@ app.get("/", (request, response) => {
 });
 
 
+// Catches routes that do not match
+app.get("*", (request, response) => {
+	console.log("User tried to visit " + request.path);
+	response.json({
+		message:"Page not found.",
+		attemptedPath: request.path
+	});
+});
+
+
+// Catch all error occurred
+app.use((error, request, response, next) => {
+	console.log("Error occured in the server.");
+	console.log(JSON.stringify(error));
+	response.json({
+		message: error.message
+	});
+});
+
 module.exports = {
 	app
 }
