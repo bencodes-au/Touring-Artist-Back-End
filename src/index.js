415e07103b0c6e0d5ad09440441d587c26fa98b0
ben_gorman <bengorman1994@gmail.com>
2025-03-13 22:09:06 +1100

    fix: deployment take 5

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const mongoose = require("mongoose");
 app.use("/venues", venueRouter);
 
 // consider moving port functionality into main.js which will control database connections
-const PORT = import.meta.env.VITE_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // connects the db on app start
 app.listen(PORT, async () => {
   console.log("Server is running on port " + PORT);
-  await mongoose.connect(import.meta.env.VITE_DATABASE_URL);
+  await mongoose.connect(process.env.DATABASE_URL);
   console.log("Database Connected");
 });
