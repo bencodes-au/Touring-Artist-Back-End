ba296b06a45d49d9318e603be37518ab99a07ee8
ben_gorman <bengorman1994@gmail.com>
2025-03-13 22:01:07 +1100

    fix: cors vite_

--- src/database.js
+++ src/database.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function databaseConnect() {
-    try {
-        const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/touring_artist_db';
-        console.log("Starting database connection!");
-        await mongoose.connect(connectionString);
-        console.log("Connected to database!");
-    } catch (error) {
-        console.error("Error connecting to database:", error);
-    }
+  try {
+    const connectionString = import.meta.env.VITE_DATABASE_URL;
+    console.log("Starting database connection!");
+    await mongoose.connect(connectionString);
+    console.log("Connected to database!");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
 }
 
 module.exports = { databaseConnect };
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const mongoose = require("mongoose");
 app.use("/venues", venueRouter);
 
 // consider moving port functionality into main.js which will control database connections
-const PORT = process.env.PORT || 3000;
+const PORT = import.meta.env.VITE_PORT || 3000;
 
 // connects the db on app start
 app.listen(PORT, async () => {
   console.log("Server is running on port " + PORT);
-  await mongoose.connect("mongodb://127.0.0.1:27017/touring_artist_db");
+  await mongoose.connect(import.meta.env.VITE_DATABASE_URL);
   console.log("Database Connected");
 });
